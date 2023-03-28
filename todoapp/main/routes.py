f932eaf61b95f8f2a6a21222743e20f1d9814e40
SHjalilo <aaaabdjalil@gmail.com>
2023-03-28 22:47:55 +0000

    sec-com

--- todoapp/main/routes.py
+++ todoapp/main/routes.py
@@ -1,13 +1,17 @@
-from flask import Blueprints , render_template , blueprints
+from flask import Blueprint , render_template ,redirect , url_for , request
 # from gevent.pywsgi import WSGIServer
 
-main = Blueprints('main', __name__)
+main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('./index.html')
 
 #from waitress import serve
 #serve(main, host="0.0.0.0", port=8080)
 # http_server = WSGIServer(("127.0.0.1", 8080), main)
 # http_server.serve_forever()
+
+@main.route('/add_todo' , methods=["POST"])
+def add_todo():
+    return redirect(url_for("main.index"))
